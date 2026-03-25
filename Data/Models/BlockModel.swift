df10ef57451f652f3efa5170e1383ca8ae98a035
Mark20110927 <13261317705@sina.cn>
2026-03-25 21:14:24 +0800

    feat: update BlockModel fields to match Block domain entity

--- Data/Models/BlockModel.swift
+++ Data/Models/BlockModel.swift
@@ -11,6 +11,12 @@ import SwiftData
 @Model
 class BlockModel {
     var id: UUID = UUID()
-    
+    var nodeID: UUID = UUID()
+    var type: String = BlockType.text.rawValue
+    var content: String = ""
+    var sortIndex: Double = 0
+    var createdAt: Date = Date()
+    var updatedAt: Date = Date()
+
     init() {}
 }
