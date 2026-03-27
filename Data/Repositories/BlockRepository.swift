75b0a6444837768adb14372b3452ee40ae9542be
Mark20110927 <13261317705@sina.cn>
2026-03-27 21:25:27 +0800

    feat: implement NodeRepository and BlockRepository skeletons with cascade delete stubs

--- Data/Repositories/BlockRepository.swift
+++ Data/Repositories/BlockRepository.swift
@@ -5,6 +5,7 @@
 //  Created by yuzheyuan on 2026/3/24.
 //
 
+import Foundation
 import SwiftData
 
 class BlockRepository: BlockRepositoryProtocol {
@@ -12,5 +13,29 @@ class BlockRepository: BlockRepositoryProtocol {
 
     init(context: ModelContext) {
         self.context = context
+    }
+
+    func fetchAll(in nodeID: UUID) throws -> [Block] {
+        throw RepositoryError.notImplemented
+    }
+
+    func fetch(by id: UUID) throws -> Block? {
+        throw RepositoryError.notImplemented
+    }
+
+    func create(_ block: Block) throws {
+        throw RepositoryError.notImplemented
+    }
+
+    func update(_ block: Block) throws {
+        throw RepositoryError.notImplemented
+    }
+
+    func delete(by id: UUID) throws {
+        throw RepositoryError.notImplemented
+    }
+
+    func deleteAll(in nodeID: UUID) throws {
+        throw RepositoryError.notImplemented
     }
 }
--- Data/Repositories/NodeRepository.swift
+++ Data/Repositories/NodeRepository.swift
@@ -5,6 +5,7 @@
 //  Created by yuzheyuan on 2026/3/24.
 //
 
+import Foundation
 import SwiftData
 
 class NodeRepository: NodeRepositoryProtocol {
@@ -12,5 +13,29 @@ class NodeRepository: NodeRepositoryProtocol {
 
     init(context: ModelContext) {
         self.context = context
+    }
+
+    func fetchAll(in pageID: UUID) throws -> [Node] {
+        throw RepositoryError.notImplemented
+    }
+
+    func fetch(by id: UUID) throws -> Node? {
+        throw RepositoryError.notImplemented
+    }
+
+    func create(_ node: Node) throws {
+        throw RepositoryError.notImplemented
+    }
+
+    func update(_ node: Node) throws {
+        throw RepositoryError.notImplemented
+    }
+
+    func delete(by id: UUID) throws {
+        throw RepositoryError.notImplemented
+    }
+
+    func deleteAll(in pageID: UUID) throws {
+        throw RepositoryError.notImplemented
     }
 }
