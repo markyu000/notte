8b39224555fdfc5cf66b08c7d4791413a53e9cb8
Mark20110927 <13261317705@sina.cn>
2026-03-27 21:25:05 +0800

    feat: implement PageRepository skeleton with notImplemented stubs

--- Data/Repositories/PageRepository.swift
+++ Data/Repositories/PageRepository.swift
@@ -5,6 +5,7 @@
 //  Created by yuzheyuan on 2026/3/24.
 //
 
+import Foundation
 import SwiftData
 
 class PageRepository: PageRepositoryProtocol {
@@ -12,5 +13,25 @@ class PageRepository: PageRepositoryProtocol {
 
     init(context: ModelContext) {
         self.context = context
+    }
+
+    func fetchAll(in collectionID: UUID) throws -> [Page] {
+        throw RepositoryError.notImplemented
+    }
+
+    func fetch(by id: UUID) throws -> Page? {
+        throw RepositoryError.notImplemented
+    }
+
+    func create(_ page: Page) throws {
+        throw RepositoryError.notImplemented
+    }
+
+    func update(_ page: Page) throws {
+        throw RepositoryError.notImplemented
+    }
+
+    func delete(by id: UUID) throws {
+        throw RepositoryError.notImplemented
     }
 }
