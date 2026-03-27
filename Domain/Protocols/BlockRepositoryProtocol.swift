623976b5edfe9d67be2712f7e7f9b2f28efeac1c
Mark20110927 <13261317705@sina.cn>
2026-03-27 21:02:30 +0800

    feat: define BlockRepositoryProtocol with CRUD and cascade delete methods

--- Domain/Protocols/BlockRepositoryProtocol.swift
+++ Domain/Protocols/BlockRepositoryProtocol.swift
@@ -5,4 +5,13 @@
 //  Created by yuzheyuan on 2026/3/24.
 //
 
-protocol BlockRepositoryProtocol {}
+import Foundation
+
+protocol BlockRepositoryProtocol {
+    func fetchAll(in nodeID: UUID) throws -> [Block]
+    func fetch(by id: UUID) throws -> Block?
+    func create(_ block: Block) throws
+    func update(_ block: Block) throws
+    func delete(by id: UUID) throws
+    func deleteAll(in nodeID: UUID) throws
+}
