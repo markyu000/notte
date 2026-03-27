9f4e29fcabfbd63d3bcb781df7b827d012b38d97
Mark20110927 <13261317705@sina.cn>
2026-03-27 21:24:47 +0800

    feat: implement CollectionRepository skeleton with notImplemented stubs

--- Data/Repositories/CollectionRepository.swift
+++ Data/Repositories/CollectionRepository.swift
@@ -5,6 +5,7 @@
 //  Created by yuzheyuan on 2026/3/24.
 //
 
+import Foundation
 import SwiftData
 
 class CollectionRepository: CollectionRepositoryProtocol {
@@ -12,5 +13,25 @@ class CollectionRepository: CollectionRepositoryProtocol {
 
     init(context: ModelContext) {
         self.context = context
+    }
+
+    func fetchAll() throws -> [Collection] {
+        throw RepositoryError.notImplemented
+    }
+
+    func fetch(by id: UUID) throws -> Collection? {
+        throw RepositoryError.notImplemented
+    }
+
+    func create(_ collection: Collection) throws {
+        throw RepositoryError.notImplemented
+    }
+
+    func update(_ collection: Collection) throws {
+        throw RepositoryError.notImplemented
+    }
+
+    func delete(by id: UUID) throws {
+        throw RepositoryError.notImplemented
     }
 }
