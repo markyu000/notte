d805bdb1edaadeb595c6b1b25868052ea16f4ef1
Mark20110927 <13261317705@sina.cn>
2026-03-28 11:16:56 +0800

    fix: rename error enum to AppError and move to AppError.swift

---
+++ Infrastructure/AppError.swift
@@ -0,0 +1,22 @@
+//
+//  AppErrorPresenter.swift
+//  Notte
+//
+//  Created by yuzheyuan on 2026/3/28.
+//
+
+import Foundation
+
+enum AppError: LocalizedError {
+    case repositoryError(RepositoryError)
+    case unknown(Error)
+
+    var errorDescription: String? {
+        switch self {
+        case .repositoryError(let e):
+            return "数据操作失败：\(e)"
+        case .unknown(let e):
+            return "未知错误：\(e.localizedDescription)"
+        }
+    }
+}
