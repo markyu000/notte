5e2904a297aca07fc9ff8aee073ea15140d21bd5
Mark20110927 <13261317705@sina.cn>
2026-03-28 11:51:08 +0800

    chore: restructure repository root to project level

---
+++ NotteTests/NotteTests.swift
@@ -0,0 +1,16 @@
+//
+//  NotteTests.swift
+//  NotteTests
+//
+//  Created by yuzheyuan on 2026/3/23.
+//
+
+import Testing
+
+struct NotteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
