8dda65f32ff43b3007ac2512ff6331c31b095c52
Mark20110927 <13261317705@sina.cn>
2026-04-01 21:14:02 +0800

    feat: add CollectionEmptyState placeholder
    #55 #57

---
+++ Notte/Features/Collections/Components/CollectionEmptyState.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionEmptyState.swift
+//  Notte
+//
+//  Created by yuzheyuan on 2026/4/1.
+//
+
+import SwiftUI
+
+struct CollectionEmptyState: View {
+    let onCreateTapped: () -> Void
+
+    var body: some View {
+        Text("No Collections")
+    }
+}
