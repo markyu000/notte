34d232efba1847decf4ccbd5886ce6c7a41e3259
markyu000 <yuzheyuan110927@icloud.com>
2026-04-09 19:46:27 +0800

    feat: add FetchPagesByCollectionUseCase
    #74

---
+++ Notte/Features/Pages/UseCases/FetchPagesByCollectionUseCase.swift
@@ -0,0 +1,17 @@
+//
+//  FetchPagesByCollectionUseCase.swift
+//  Notte
+//
+//  Created by 余哲源 on 2026/4/9.
+//
+
+import Foundation
+
+struct FetchPagesByCollectionUseCase {
+    let repository: PageRepositoryProtocol
+    
+    func excute(collectionID: UUID) async throws -> [Page] {
+        let all = try await repository.fetchAll(in: collectionID)
+        return all.sorted { $0.sortIndex < $1.sortIndex }
+    }
+}
