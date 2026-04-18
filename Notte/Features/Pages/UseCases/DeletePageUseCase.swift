63644ce01750755688b155130453b93097092495
余哲源 <zheyuanyu@192.168.1.15>
2026-04-18 22:16:13 +0800

    fix: restore the normal delete process of nodes and blocks

--- Notte/Features/Pages/UseCases/DeletePageUseCase.swift
+++ Notte/Features/Pages/UseCases/DeletePageUseCase.swift
@@ -15,7 +15,7 @@ struct DeletePageUseCase {
     func execute(pageID: UUID) async throws {
         logger.debug("开始删除 Page, id=\(pageID)", function: #function)
         // 1. 先级联删除该 Page 下的所有 Node
-        try? await nodeRepository.deleteAll(in: pageID)
+        try await nodeRepository.deleteAll(in: pageID)
         // 2. 再删除 Page 本身
         try await repository.delete(by: pageID)
         logger.info("Page 删除成功, id=\(pageID)", function: #function)
